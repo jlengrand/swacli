99c12df5498d450fd3a2b49c65f8046954d0c163
Julien Lengrand-Lambert <Julien@lengrand.fr>
2022-01-20 15:54:36 +0100

    Generate executable

---
+++ buildNativeImage.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+/Users/julienlengrand-lambert/Downloads/graalvm-ce-java11-21.3.0/Contents/Home/bin/native-image -jar ./build/libs/all-in-one-jar-1.0-SNAPSHOT.jar sw --enable-http --enable-https
